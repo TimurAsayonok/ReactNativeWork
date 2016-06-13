270a247ecff51682339668043707cf9a27fe0a68
Timur Asayonok <timurasayonok@MacBook-Pro-Timur.local>
2016-06-14 00:34:18 +0300

    Start work

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -5,44 +5,23 @@
  */
 
 import React, { Component } from 'react';
-import Main from './App/Components/Main';
 import Button from 'react-native-button';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   TextInput,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
   TouchableHighlight,
   View,
   WebView,
-  AlertIOS,
-  NavigatorIOS
 } from 'react-native';
 
 
 class AwesomeProject extends Component {
-  _onPressButtonGet(){
-    fetch("http://localhost:3000/test?search=nraboy", {method: "GET"})
-    .then((response) => response.json())
-    .then((responseData) =>{
-      AlertIOS.alert("GET Response",
-                "Search Query -> " + responseData.search)
-    })
-    .done();
-  }
-  _handlePress() {
-    console.log('Pressed!');
-  }
+
   render() {
     return (
-      <NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: 'Github Notetaker',
-          component: Main
-        }} />
+      <Text>Test work</Text>
     );
   }
 }
